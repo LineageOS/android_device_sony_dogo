9aeaab6da7cfc598b571f9b462edbbf20d533474
CyberWalkMaN <firefox883@gmail.com>
2017-03-28 00:49:35 +0500

    dogo: update build fingerprint & description
    * From 10.7.A.0.228
    
    Change-Id: I4061486da1ee0d098e86b2108c72a2fcb01faae4

--- lineage.mk
+++ lineage.mk
@@ -3,7 +3,7 @@ $(call inherit-product, device/sony/dogo/full_dogo.mk)
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C5503 BUILD_FINGERPRINT=Sony/C5503/C5503:5.1.1/10.7.A.0.222/1314554057:user/release-keys PRIVATE_BUILD_DESC="C5503-user 5.1.1 10.7.A.0.222 1314554057 test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C5503 BUILD_FINGERPRINT=Sony/C5503/C5503:5.1.1/10.7.A.0.228/58103698:user/release-keys PRIVATE_BUILD_DESC="C5503-user 5.1.1 10.7.A.0.228 58103698 release-keys"
 
 PRODUCT_NAME := lineage_dogo
 PRODUCT_DEVICE := dogo
