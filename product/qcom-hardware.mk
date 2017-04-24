ced1c37b5ffa7ec782f94dce395195894d9189ea
Chippa-a <vusal1372@gmail.com>
2017-04-24 22:04:33 +0300

    dogo: Correct AAPT configuration
     * Dogo display - 4.55" / 720 x 1280 px, xxhdpi is wrong for these params
     * Update the dalvik-heap and hwui configs as well
     * HWUI configuration values taken from commit
       I73357a35bee22d76d3fe8a98ece91c5c9d19371d by Steve Kondik
    
    Change-Id: I963e9372e5405e0eb1ec68dddf509abffd0187e4

--- product/qcom-hardware.mk
+++ product/qcom-hardware.mk
@@ -1,3 +1,17 @@
 # Hardware permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+
+# Hardware User Interface properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.texture_cache_size=48 \
+    ro.hwui.layer_cache_size=32 \
+    ro.hwui.r_buffer_cache_size=4 \
+    ro.hwui.path_cache_size=24 \
+    ro.hwui.gradient_cache_size=1 \
+    ro.hwui.drop_shadow_cache_size=5 \
+    ro.hwui.texture_cache_flushrate=0.5 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=1024 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=1024
